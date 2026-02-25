4d82e0648afa47605da3297ed16884ddead3ce51
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2026-02-25 14:10:26 +0700

    HN_K24_CNTT5_HoKhanhLinh_003
    Count letters, digits, and special characters in input.

--- HN-K24-CNTT5_HoKhanhLinh_003/hackathon/src/HN_K24_CNTT5_HoKhanhLinh_003.java
+++ HN-K24-CNTT5_HoKhanhLinh_003/hackathon/src/HN_K24_CNTT5_HoKhanhLinh_003.java
@@ -6,6 +6,24 @@ public class HN_K24_CNTT5_HoKhanhLinh_003 {
     String number = "";
     System.out.printf("Nhap vao ki tu :");
     number = sc.nextLine();
+        int letters = 0;
+        int digits = 0;
+        int special = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                letters++;
+            } 
+            else if (Character.isDigit(ch)) {
+                digits++;
+            } 
+            else {
+                special++; 
+                
+            }
+        }
 
 
 
