0b0a2c087cc022338c18624fb2e1f5c5cc53be7e
hokhanhlinh2006 <hokhanhlinh2006@gmail.com>
2026-02-25 14:14:27 +0700

    Cau12.java

--- HN-K24-CNTT5_HoKhanhLinh_003/hackathon/src/Cau12.java
+++ HN-K24-CNTT5_HoKhanhLinh_003/hackathon/src/Cau12.java
@@ -1,4 +1,11 @@
 //Cau 1.2: Nhap vao mot chuoi ky tu la ho ten day du tu ban phim
+import java.util.Scanner;
 public class Cau12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Nhap vao ho va ten: ");
+        String name = sc.nextLine();
+        System.out.println("Ban vua nhap: " + name);
+    }
 }
